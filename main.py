6e82a11aa6c4b7fa14ae50da0d6c8c8c02efce8b
mallobcd <m2bc2d.pro@gmail.com>
2025-10-24 11:49:47 +0200

    fix detect win

--- main.py
+++ main.py
@@ -1,28 +1,9 @@
-import tkinter as tk
 from PySide6.QtWidgets import QApplication
+from presenters.main_presenter import MainPresenter
+from ui.main_view import MainView
 
-from camera.camera_switch import launch_camera
-from ui.ui import Ui
-
-qapp = QApplication()
-ui = Ui()
-ui.setup_ui()
-
-root = tk.Tk()
-root.title("Sélection de Caméra (Index / Flux Réseau)")
-
-label = tk.Label(root, text="Choisis le type de caméra :", font=("Arial", 12))
-label.pack(pady=10)
-
-btn_index = tk.Button(root, text="Caméra Index", command=lambda: launch_camera("index"), width=25)
-btn_index.pack(pady=5)
-
-btn_flow = tk.Button(root, text="Caméra Flow", command=lambda: launch_camera("flow"), width=25)
-btn_flow.pack(pady=5)
-
-btn_quit = tk.Button(root, text="Quitter", command=root.destroy, width=25)
-btn_quit.pack(pady=10)
-
-root.mainloop()
-
+if __name__ == "__main__":
+    q_app = QApplication()
+    view = MainView()
+    MainPresenter(view)
 
